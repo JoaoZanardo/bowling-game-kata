5160056fbc3debfe33a66158d67a65c3bc40983e
João Zanardo <joaozzz0105@gmail.com>
2022-10-15 17:22:23 -0300

    init

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
+};
